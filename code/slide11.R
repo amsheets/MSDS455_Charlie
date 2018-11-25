897cc485e86d393fb3bfda4d14e4f33764fa336a
Anna Sheets <asheets@ASheets.local>
2018-11-25 16:35:33 -0700

    slide 11

---
+++ code/slide11.R
@@ -0,0 +1,86 @@
+#Install Packages
+list.of.packages <- c("ggplot2", "ggthemes")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+lapply(list.of.packages, require, character.only = TRUE)
+
+## theme
+
+# https://material.io/design/color/the-color-system.html#tools-for-picking-colors
+pal_heart <- c("#FF8A80", "#D50000", "#FF1744",  "#FF5252", "#B71C1C","#7f0000")
+blue_heart <- "#3F69AA"
+
+############# color pieces!
+scale_fill_heart <- function(){
+  
+  structure(list(
+    scale_fill_manual(values=pal_heart)
+  ))
+}
+
+scale_color_discrete_heart <- function(){
+  
+  structure(list(
+    scale_color_manual(values=pal_heart)
+  ))
+}
+
+scale_color_continuous_heart <- function(){
+  
+  structure(list(
+    scale_color_gradientn(colours = pal_heart)
+  ))
+}
+##################################
+
+theme_heart <- function(base_size = 20, base_family = "Helvetica",
+                        base_line_size = base_size / 42,
+                        base_rect_size = base_size / 42) {
+  # Starts with theme_bw and remove most parts
+  theme_bw(
+    base_size = base_size,
+    base_family = base_family,
+    base_line_size = base_line_size,
+    base_rect_size = base_rect_size
+  ) %+replace%
+    theme(
+      axis.ticks      = element_blank(),
+      axis.text       = element_text(size = 16,color=blue_heart),
+      axis.title = element_text(color = blue_heart),
+      legend.background = element_blank(),
+      legend.key        = element_blank(),
+      legend.title = element_blank(),
+      panel.background   = element_blank(),
+      panel.border      = element_blank(),
+      strip.background  = element_blank(),
+      plot.title = element_text(color = blue_heart, face = "bold"),
+      plot.background   = element_blank(),
+      
+      complete = TRUE
+    )
+}
+
+# Set working directory
+setwd("~/Documents/GradSchool/MSDS455/git_repos/MSDS455_Charlie/data")
+
+# read in csv file
+heart_disease_deaths <- 
+  read.csv(file = "deaths-by-heart-diseases.csv", 
+           stringsAsFactors = FALSE)
+
+#check data
+str(heart_disease_deaths)
+
+# Create Plot
+setwd("~/Documents/GradSchool/MSDS455/git_repos/MSDS455_Charlie/visualizations")
+png(file = "slide_11_HD_deaths_over_time.png",width = 1000, height = 1000) 
+p <- ggplot(data=heart_disease_deaths,aes(x=Year,y=Death.rate.per.100.000.population))+
+  geom_line(size=1.1, color = "#FF8A80")+
+  guides(linetype=F)+
+  scale_x_continuous(breaks = seq(1950, 2016, by = 5))+
+  labs(x="Year",y="Death Rate per 100,000 Population") +
+  scale_color_discrete_heart() + 
+  theme_heart() + 
+  ggtitle("Deaths by heart diseases in the U.S. from 1950 to 2016")
+print(p)
+dev.off()
